fa5500f9b090aca8120dc00ed5c5c3e69ce77f4b
aykaylone-droid <aykaylone@gmail.com>
2026-08-21 12:12:02 +0200

    updated

--- email.py
+++ email.py
@@ -1,6 +1,6 @@
 import os, json, base64, cohere
+import pickle
 
-from google.auth.api_key import Credentials
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
@@ -12,21 +12,24 @@ co = cohere.Client("HCYnZ0Csxn4dTZdQB0WV2q4K0NJ9uzgzJa4EJd4G")
 SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
 
 def get_gmail():
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
 
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES) \
-        if os.path.exists("token.json") else None
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                "credentials.json", SCOPES
-            )
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
             creds = flow.run_local_server(port=0)
 
-        with open('token.json', 'w') as f:
-            f.write(creds.to_json())
+    with open('token.pickle', 'wb') as token:
+        pickle.dump(creds, token)
 
+        service = build('gmail', 'v1', credentials=creds)
+
+        return service.users().messages().list(userId='me',maxResults = 2).execute()
 
 gmail = get_gmail()
 print("Gmail connected!")
